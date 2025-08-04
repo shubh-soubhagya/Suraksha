a3ebb8982b179d63182d666344d89a144ca38483
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-08-05 00:59:37 +0530

    config pushed

---
+++ static/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDk2WQYOW_KEoGv7oBEdFhI7nu-U35vTXE",
+  authDomain: "suraksha-d8e67.firebaseapp.com",
+  projectId: "suraksha-d8e67",
+  storageBucket: "suraksha-d8e67.appspot.com",
+  messagingSenderId: "832078576907",
+  appId: "1:832078576907:web:d51e7039aacbc442dbb60e"
+};
+
+firebase.initializeApp(firebaseConfig);
